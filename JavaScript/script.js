ae32dcceaa4cc86a63d3492d2f52e72b945c7f14
Belug04 <166060127+Belug04@users.noreply.github.com>
2024-09-10 19:07:06 -0300

    act4

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,33 +1,11 @@
-function sumar(num1,num2)
-{
-    let resultado= num1 + num2;
-    console.log(resultado)
+let edad= prompt("Para comprar en esta pagian necesita ser mayor de edad. Ingrese su edad: ");
+
+function mayor(edad){
+    if(edad >= 18){
+        alert("Exitos en su compra.");
+    }else{
+        alert("Puede comprar con la supervisión de un adulto. Puede seguir navegando.")
+    }
 }
 
-sumar(20,10)
-
-function nombres(nombre)
-{
-    console.log("Hola, " + nombre + " como estas?");
-}
-
-nombres("Belen")
-
-function mtsCuadrados(largo, ancho)
-{
-    let mts= largo * ancho;
-    console.log(mts + "m2");
-}
-
-mtsCuadrados(6,1)
-mtsCuadrados(3,5)
-
-function areacirculo(diametro)
-{
-    let radio= diametro/2;
-    let area= 3.14 * (radio*radio);
-    console.log("El area del circulo es: " + area);
-
-}
-
-areacirculo(850)
+mayor(edad);
